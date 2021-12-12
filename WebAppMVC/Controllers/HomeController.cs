8c317207c78e14b863861a97f6aba4e749458512
Rajhans kumar <rajchaurasia@hotmail.com>
2021-12-12 18:22:05 +0530

    Add project files.

---
+++ WebAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppMVC.Controllers
+{
+    [RoutePrefix("Home")]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("~/About/{id}/{name?}")]
+        public ActionResult AboutAction(int id, string name)
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View("About");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
